01fd4da9569a73ad1f4d4aebf79137b6093d5634
Yvette Nartey <newerkey@gmail.com>
2023-10-05 14:42:05 +0200

    feat(views): generate views for register and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root "home#index"
 
+  get "/home", to: "home#index"
+  get "/register", to: "register#index"
+  get "/login", to: "login#index"
+
   get "/facts", to: "facts#index"
+
 end
